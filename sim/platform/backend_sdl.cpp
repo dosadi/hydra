0950298ffb8b5a9aeac37feb204fd5d02527cefc
Jonathan Mor Taylor <dosadi82@gmail.com>
2025-11-22 12:35:22 -0500

    Add SDL backend ops scaffold (stub present)

--- sim/platform/backend_sdl.cpp
+++ sim/platform/backend_sdl.cpp
@@ -1,5 +1,15 @@
 #include "backend_ops.h"
 
 BackendOps get_ops_sdl() {
-    return make_stub_ops();
+    BackendOps ops;
+    ops.init = [](PlatformContext& ctx, const PlatformConfig&) -> bool {
+        // Nothing to init yet; keep stub user handle
+        ctx.user = reinterpret_cast<void*>(0x1);
+        return true;
+    };
+    ops.present = [](PlatformContext&, const uint32_t*, int, int) {
+        // TODO: wire to SDL texture present; stub for now
+    };
+    ops.shutdown = [](PlatformContext&) {};
+    return ops;
 }
